da6c02e92347aa17a8568d14eb9f4a899049dae0
samuelguedesalves <guedes.works7@gmail.com>
2021-04-10 12:00:24 -0400

    create clients component and add images inner

---
+++ src/components/Clients/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Logos } from './constants'
+
+import {
+    ClientsSection as Section,
+} from './styles'
+
+const Clients: React.FC = () => {
+    return (
+        <Section>
+            <div className="container">
+                <div className="logo-list">
+                    {
+                        Logos.map( logo => {
+                            console.log(logo); 
+                            return<img src={logo} alt="logo do cliente"/>
+                        } )
+                    }
+                </div>
+            </div>
+        </Section>
+    )
+}
+
+export default Clients;
---
+++ src/components/Clients/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ClientsSection = styled.section`
+    background: linear-gradient(111.34deg, #545454 0%, #0B0B0B 100%);
+
+    .container {
+        width: 100%;
+        max-width: 1032px;
+        margin: 0 auto;
+
+        .logo-list {
+            display: grid;
+            grid-template-columns: repeat( auto-fit, minmax(152px, 1fr) );
+        }
+    }
+`;
