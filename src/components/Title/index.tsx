8fffafd86a3c5e8efd277375880136ac8661c95b
Diego-Lopes <diegolopes2009@hotmail.com>
2021-04-10 12:09:08 -0300

    adição de nova pasta Title e adição nova section index da pasta page.

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {NameTitulo} from './styles';
+
+const Title: React.FC = ({}) => {
+    return(
+        <>
+            <NameTitulo>Nossos Serviços</NameTitulo>
+        </>
+
+
+    )
+
+}
+
+export default Title;
---
+++ src/components/Title/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const NameTitulo = styled.h1`
+    font-size: 32px;
+    font-weight: 700;
+    font-family: "Inter";
+    text-align: center;
+    color: rgba(223, 223, 223, 1);
+`
