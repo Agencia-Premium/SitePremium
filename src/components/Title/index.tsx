3cda8944a4e0ad64bf4b351e2e6ce3d648d5ae92
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-04-12 12:45:33 -0300

    update component title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+type TitleProps = {
+    name: string
+}
+
 import {NameTitulo, LineBotton, SubTitulo} from './styles';
 
-const Title: React.FC = ({}) => {
+const Title: React.FC<TitleProps> = ({ name }) => {
     return(
         <>
             <LineBotton>
-                <NameTitulo>Nossos Serviços</NameTitulo>
+                <NameTitulo>{name}</NameTitulo>
                 <img src="line_botton.svg"/>
                 <SubTitulo>Desempenhamos diversas atividades e funções que tem como objetivo trazer seu negócio para o online, ou fazer com que ele ganha mais força e atraia potênciais clientes.</SubTitulo>
             </LineBotton>
