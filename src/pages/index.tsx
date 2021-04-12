5b7be83b09a81c1dbf98a6b9385106400db0021a
Diego-Lopes <diegolopes2009@hotmail.com>
2021-04-12 09:32:23 -0300

    adição dos componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+// importar os componentes
 import Head from 'next/head'
 import NavBar from '../components/NavBar'
 import Header from '../components/Header'
-import GlobalStyle from '../styles/global'
+import GlobalStyle from '../styles/global'//estilo global.
 import Title from '../components/Title'
+import Cardes from '../components/Cardes'
+import TitleMid from '../components/TitleMid'
+import CardesClient from '../components/CardesClient'
 
 export default function Home() {
   return (
@@ -21,8 +25,18 @@ export default function Home() {
           <Header></Header>
         </main>
         <section> 
-            <Title></Title>
+          <Title></Title>
+          <Cardes></Cardes>
         </section>
+          
+        <section>
+          <TitleMid></TitleMid>
+        </section>
+
+        <section>
+          <CardesClient></CardesClient>
+        </section>
+
         <footer>
           
         </footer>
