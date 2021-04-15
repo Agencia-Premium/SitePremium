28a68a3d6d4926a64c7820a18b4a6ab7af4dd464
Diego-Lopes <diegolopes2009@hotmail.com>
2021-04-15 15:51:36 -0300

    ajuste na pg contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import NavBar from "../components/NavBar";
 import TitleForm from "../components/TitleForm";
 import ContactFooter from "../components/ContactFooter";
+import Input from "../components/Input";
 
 export default function contact() {
     return (
@@ -19,11 +20,18 @@ export default function contact() {
             </header>
             
             <main>
+
                 <TitleForm
                     name="Formulário de Contato"
                     description="Preencha o formulário e nossa equipe estrará 
                     em contato com você em até 24 horas."
                 />
+                <div className="conteinerForme">
+                    <Input background="transparent" placeholder="Qual é seu nome?"/>
+                    <Input background="transparent" placeholder="Seu e-mail"marginBottom={false}/>
+                    <Input background="transparent" placeholder="Seu telefone."/>
+                    <Input background="transparent" placeholder="O que você precisa?" scale="large"/>
+                </div>
             </main>
 
             <footer>
