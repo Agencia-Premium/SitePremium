49bfb0032e074b5811654ce88c240e265b575c4f
Diego-Lopes <diegolopes2009@hotmail.com>
2021-04-17 13:41:59 -0300

    correção do arquivo sobre.tsx

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React from "react";
 import NavBar from "../components/NavBar";
 import GlobalStyle from "../styles/global";
 import Footer from "../components/Footer";
 import HeaderSobre from "../components/HeaderSobre";
+import CardsSobre from "../components/CardsSobre";
+
 export default function sobre() {
-    return(
-        <div className="container">
-            <GlobalStyle/>
-            <head>
-                <title>Sobre Premium</title>
-                <link rel="icon" href="/favicon.ico" />
-            </head>
+  return (
+    <div className="container">
+      <GlobalStyle />
+      <head>
+        <title>Sobre Premium</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
 
-            <header>
-                <NavBar/>
-                <HeaderSobre/>
-            </header>
+      <header>
+        <NavBar />
+        <HeaderSobre />
+      </header>
 
-            <main>
+      <main className="CardsSobre">
+        <CardsSobre
+          image="valores.svg"
+          title="Nossos Valores:"
+          subtitle="Valorização dos nossos colaboradores; Ter Ética e Princípios acima de tudo; Promover a inovação e o uso de novas tecnologias como cerne do nosso negócio;"
+          alt = "Nossos Valores"            
+        />
+        <CardsSobre
+          image="missao.svg"
+          title="Nossos Missão:"
+          subtitle="Promover a resolução de problemas de nossos clientes e da sociedade por meio do Marketing, da Tecnologia e da Inovação com agilidade, ética e transparência."
+          alt = "Nossos Missão"            
+        />
+      </main>
 
-            </main>
-
-            <footer>
-                <Footer/>
-            </footer>
-        </div>
-        
-    )
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
