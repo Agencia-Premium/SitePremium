bade33a0d5a23fb20bdea96b9543f6ca2456a94c
Diego-Lopes <diegolopes2009@hotmail.com>
2021-04-17 13:40:02 -0300

    adicionando colaborador

--- src/components/Collaborator/constants/index.ts
+++ src/components/Collaborator/constants/index.ts
@@ -12,15 +12,15 @@ export const Collaborators = [
         }
     },
     {
-        image: './collaborators/gustavo.jpg',
-        name: 'Gustavo Castilho',
+        image: './collaborators/Diego.png',
+        name: 'Diego da Silva Lopes',
         occupation: 'Programador Full Stack',
         description: 'Transformar interfaces em códigos altamente funcionais usando a linguagem adequada.',
         socialMedia: {
-            facebook: 'https://www.facebook.com/gustavo.castilho.161',
-            instagram: 'https://www.instagram.com/gustavocvalenciano/',
-            linkedin: 'https://www.linkedin.com/in/gustavo-castilho-914a621b4/',
-            twitter: 'https://twitter.com/gustavocvalenc1'
+            facebook: 'https://www.facebook.com/DiegoLopes2009/',
+            instagram: 'https://www.instagram.com/acad.diegolopes/',
+            linkedin: 'https://www.linkedin.com/in/diego-lopes-37877a105/',
+            twitter: 'https://twitter.com/diegolopes2009'
         }
     },
     {
