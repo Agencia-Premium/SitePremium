cc9bdb94a0800e1e91e409949177404de85fe867
unknown <diegolopes2009@hotmail.com>
2021-04-23 11:13:48 -0300

    created a new components

---
+++ src/components/ImageProject/constants/index.ts
@@ -0,0 +1,40 @@
+export const Card = [
+    {
+        imagem: "ceu.png",
+        titulo:"Design",
+        subTitulo:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    }, 
+    {
+        imagem: "predio.svg",
+        titulo:"teste",
+        subTitulo:"teste teste",
+    },
+    {
+        imagem: "ceu.png",
+        titulo:"teste1",
+        subTitulo:"teste1 teste2",
+    },
+
+    {
+        imagem: "ceu.png",
+        titulo:"teste",
+        subTitulo:"teste teste",
+    },
+    {
+        imagem: "predio.svg",
+        titulo:"teste",
+        subTitulo:"teste teste",
+    },
+    // {
+    //     imagem: "ceu.png",
+    //     titulo:"teste1",
+    //     subTitulo:"teste1 teste2",
+    // },
+    {
+        imagem: "ceu.png",
+        titulo:"Design",
+        subTitulo:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+
+
+]
