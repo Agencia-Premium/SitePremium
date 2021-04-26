1181d3bbe74df23419c13e34352ae783bad6fb12
unknown <diegolopes2009@hotmail.com>
2021-04-26 10:41:31 -0300

    code improvement

--- src/components/ImageProject/index.tsx
+++ src/components/ImageProject/index.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import { ContainerGallery } from './styles';
+import { ContainerGallery } from "./styles";
 
 type ProjectProps = {
-    imagem: string;
-    titulo: string;
-    subTitulo?: string;
-    arrow?: string;
-    alt?: string;
-}
+  imagem: string;
+  titulo: string;
+  subTitulo?: string;
+  arrow?: string;
+  alt?: string;
+};
 
-const ImageProject: React.FC<ProjectProps> = ({imagem, titulo, subTitulo, arrow, alt }) => {
-    return(
-        <>
-            <ContainerGallery>
-                <div className="quadro">
-                    <div className="imagem">
-                        <img src={imagem} alt={alt} />
-                    </div>
-                    <div className="blur" />
-                    <h2>{titulo}</h2>
-                    <p>{subTitulo}</p>
-                    <img className="arrow"src={arrow} alt="seta" />
-                </div>
-                
-            </ContainerGallery>
-        </>
-    )
-}
+const ImageProject: React.FC<ProjectProps> = ({
+  imagem,
+  titulo,
+  subTitulo,
+  arrow,
+  alt,
+}) => {
+  return (
+    <>
+      <ContainerGallery>
+        <div className="quadro">
+          <div className="imagem">
+            <img src={imagem} alt={alt} />
+          </div>
+          <div className="blur" />
+          <h2>{titulo}</h2>
+          <p>{subTitulo}</p>
+          <img className="arrow" src={arrow} alt="seta" />
+        </div>
+      </ContainerGallery>
+    </>
+  );
+};
 export default ImageProject;
--- src/components/ImageProject/styles.ts
+++ src/components/ImageProject/styles.ts
@@ -1,74 +1,70 @@
 import styled from "styled-components";
 
-
 export const ContainerGallery = styled.div`
-  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+  color: #ffffff;
+
+  .quadro {
+    width: 329px;
+    height: 386px;
+    position: relative;
+    box-sizing: border-box;
+    margin: 1.2rem;
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+  .imagem {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      border-radius: 8px;
+    }
+  }
+
+  h2 {
+    position: absolute;
+    top: 272px;
+    left: 19px;
+    font-size: 3.2rem;
+    font-weight: 700;
+    font-family: "Inter";
+  }
+
+  p {
+    position: absolute;
+    top: 309px;
+    left: 20px;
+    width: 210px;
+    height: 63px;
+    font-size: 1.4rem;
+    font-weight: 400;
+    font-family: "Inter";
+    text-align: justify;
+    overflow: hidden;
+  }
+
+  .blur {
+    width: 328px;
+    height: 202px;
+    position: absolute;
+    bottom: -2px;
+    border: none;
+    background-color: rgba(0, 0, 0, 0.25);
     border-radius: 8px;
-    color: #ffffff;
-    
+    backdrop-filter: blur(10px);
+    clip-path: polygon(0% 100%, 100% 100%, 100% 39%, -12% 0%);
+  }
 
-    .quadro{
-        
-        width: 329px;
-        height: 386px;
-        position: relative;
-        box-sizing: border-box;
-        margin: 1.2rem;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        
-    }
-    .imagem {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        
-        img {
-            border-radius: 8px;
-        }
-    }
-
-    h2 {
-        position:absolute;
-        top: 272px;
-        left:19px;
-        font-size: 3.2rem;
-        font-weight: 700;
-        font-family: "Inter";
-
-    }
-    
-    p { 
-        position: absolute;
-        top: 309px;
-        left: 20px;
-        width: 210px;
-        height: 63px;
-        font-size: 1.4rem;
-        font-weight: 400;
-        font-family: "Inter";
-        text-align: justify;
-        overflow: hidden; 
-
-    }
-    
-    .blur {
-        width: 329px;
-        height: 130px;
-        position: absolute;
-        bottom: 0;
-        border: none;
-        backdrop-filter: blur(2px);
-    }
-
-    .arrow {
-        position: absolute;
-        top: 325px;
-        left: 261px;
-        cursor: pointer;
-    }
-`
+  .arrow {
+    position: absolute;
+    top: 325px;
+    left: 261px;
+    cursor: pointer;
+  }
+`;
